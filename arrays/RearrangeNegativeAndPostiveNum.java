e7cba4daeab79e92c0224d06425179b3afc13699
ullasganesh <69979279+Ullasganesh@users.noreply.github.com>
2021-09-22 18:32:44 +0530

    Add files via upload

---
+++ arrays/RearrangeNegativeAndPostiveNum.java
@@ -0,0 +1,46 @@
+package arrays;
+
+
+public class RearrangeNegativeAndPostiveNum {
+    
+
+
+
+
+
+	static void rearrange(int arr[], int n)
+	{
+		int j = 0, temp;
+		for (int i = 0; i < n; i++) {
+			if (arr[i] < 0) {
+				if (i != j) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+				j++;
+			}
+		}
+	}
+
+	// A utility function to print an array
+	static void printArray(int arr[], int n)
+	{
+		for (int i = 0; i < n; i++)
+			System.out.print(arr[i] + " ");
+	}
+
+	// Driver code
+	public static void main(String args[])
+	{
+		int arr[] = { -12, 11, -13, -5, 6, -7, 5, -3, -6 };
+		int n = arr.length;
+
+		rearrange(arr, n);
+		printArray(arr, n);
+	}
+}
+
+
+    
+
